ec48987d65d6ec8933a0b750173db4878ac4618e
Raiquen <raiqueng@live.com>
2023-04-18 22:36:27 +0200

    configured service_worker

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({hide_tweet_analytics: true})
 })
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        if(key === "hide_tweet_analytics") {
+            (async () => {
+                const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+                chrome.tabs.sendMessage(tab.id, {[key]: newValue})
+            })();
+        }
+    }
+});
---
+++ content.js
@@ -0,0 +1,10 @@
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log('MESSAGE', message);
+    if (message.hide_tweet_analytics) {
+        document.body.classList.add("hide-tweet-analytics")
+    } else {
+        document.body.classList.remove("hide-tweet-analytics")
+    }
+})
