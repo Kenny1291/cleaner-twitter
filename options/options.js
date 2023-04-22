0e856d1f2ec692b702725a5d7d82e1662ddea4d3
Raiquen <raiqueng@live.com>
2023-04-22 21:02:11 +0200

    added editor and formatCSS func

--- options/options.js
+++ options/options.js
@@ -2,7 +2,25 @@
     const CSSRules = await chrome.storage.sync.get().then(result => {
         return result.CSSRulesArrayOfObjectsWithNames
     })
-    const textArea = document.querySelector('textarea')
-    textArea.value = CSSRules.map(rule => rule.rule).join('\n')
+
+    let editor = ace.edit("editor")
+    editor.setTheme("ace/theme/twilight")
+    editor.session.setMode("ace/mode/css")
+
+    let formattedCSS = formatCSS(CSSRules.map(rule => rule.rule).join('\n'))
+
+    editor.session.insert({row: 0, column: 0}, formattedCSS)
+
+
+    // editor.session.getValue()
+
+
+    function formatCSS(css) {
+        return css.replace(/([{])\s*/g, '$1\n    ').replace(/}\s*/g, '\n}\n').replace(/([^\s])\s*{/g, '$1 {').trim();
+    }
+    
+
 })()
+
+
 
