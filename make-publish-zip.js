1d6193bb74f1cad1b773bfd0fc496a0f9ebf32ed
Raiquen <raiqueng@live.com>
2024-01-21 22:17:04 +0100

    added script to generate ZIP to publish extension

---
+++ make-publish-zip.js
@@ -0,0 +1,52 @@
+const JSZip = require('jszip')
+const fs = require('fs')
+const path = require('path')
+
+async function addFile(zip, filePath) {
+    const data = fs.readFileSync(filePath)
+    const fileName = path.basename(filePath)
+    zip.file(fileName, data)
+}
+
+async function addFolder(zip, folderPath) {
+    const files = fs.readdirSync(folderPath)
+    const folderName = path.basename(folderPath)
+    const folder = zip.folder(folderName)
+    for (const file of files) {
+        const filePath = path.join(folderPath, file)
+        if(fs.statSync(filePath).isDirectory()) {
+            await addFolder(folder, filePath)
+        } else {
+            await addFile(folder, filePath)
+        }
+    }
+}
+
+async function createZip(files, folders) {
+    const zip = new JSZip()
+
+    for (const file of files) {
+        await addFile(zip, file)
+    }
+    for (const folder of folders) {
+        await addFolder(zip, folder)
+    }
+
+    zip.generateAsync({type: 'nodebuffer'}).then((content) => {
+        fs.writeFileSync('dist/cleaner-twitter.zip', content)
+    })
+}
+
+//********************************************************************
+const filesToAdd = ['manifest.json']
+const foldersToAdd = [
+    'images',
+    'options',
+    'popup',
+    'scripts',
+    'service_workers',
+    'utils',
+    'vendor'
+]
+
+createZip(filesToAdd, foldersToAdd)
