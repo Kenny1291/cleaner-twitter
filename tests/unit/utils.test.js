ec342950e62f2caa0065723731c233b13a054810
Raiquen <raiqueng@live.com>
2024-01-26 05:53:31 +0100

    add unit tests for utils.js

---
+++ tests/unit/utils.test.js
@@ -0,0 +1,47 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import { 
+    getRuleName,
+    processCSSRule,
+    fetchDefaultCSSRulesJSON
+} from '../../utils/utils.js'
+import defaultCSSRules from '../../data/defaultCSSRules.json' assert { type: 'json' }
+import CSSRulesArrayOfObjectsWithNames from './static-data/CSSRulesArrayOfObjectsWithNames.json' assert { type: 'json' }
+
+const exampleRule = ".hide_tweet_analytics div:has(> a[aria-label$='View post analytics']) {display: none;}"
+
+describe('getRuleName()', () => {
+    it('should extract the class from a CSS rule', () => {
+        const expected = 'hide_tweet_analytics'
+        const actual = getRuleName(exampleRule)
+        assert.equal(actual, expected)
+    })
+})
+
+describe('processCSSRule()', () => {
+    it('should return a CSSRuleObject', () => {
+        const expected = CSSRulesArrayOfObjectsWithNames[0]
+        const actual = processCSSRule(exampleRule, CSSRulesArrayOfObjectsWithNames)
+        assert.deepEqual(actual, expected) 
+    })
+
+    it('should match the active property value if the rule is found', () => {
+        const expected = CSSRulesArrayOfObjectsWithNames[0].active
+        const actual = processCSSRule(exampleRule, CSSRulesArrayOfObjectsWithNames).active
+        assert.equal(actual, expected)
+    })
+
+    it('should set the active property to false if the rule is not found', () => {
+        const exampleRule = ".test_rule div:has(> a[aria-label$='thisIsATest']) {display: none;}"
+        const expected = processCSSRule(exampleRule, CSSRulesArrayOfObjectsWithNames)
+        assert.equal(false, expected.active)
+    })
+})
+
+describe('fetchDefaultCSSRulesJSON()', () => {
+    it('should return the defaultCSSRules JSON', async () => {
+        const expected = defaultCSSRules
+        const actual = await fetchDefaultCSSRulesJSON()
+        assert.deepEqual(actual, expected)
+    })
+})
