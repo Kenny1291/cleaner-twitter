19a9c4ef2966999f8a880ab3549834df7c05d6dc
Raiquen <raiqueng@live.com>
2024-01-30 18:32:23 +0100

    test: default rule validity

---
+++ tests/e2e/external/default-rules-validity.spec.js
@@ -0,0 +1,55 @@
+// @ts-ignore
+import { test, expect } from '@playwright/test'
+import { testUser } from './static-data/twitterCredentials.js'
+// @ts-ignore
+import defaultCSSRules from '../../../data/defaultCSSRules.json' assert { type: 'json' }
+
+let page
+
+test.beforeAll(async ({ browser }) => {
+    const context = await browser.newContext()
+    page = await context.newPage()
+    await page.goto('https://twitter.com')
+
+    await page.click('text="Sign in"')
+    await page.fill('input', testUser.EMAIL)
+    await page.click('text="Next"')
+
+    try {
+        await page.waitForSelector('input[name="text"]', { timeout: 1000 }),
+        await page.fill('input[name="text"]', testUser.USERNAME)
+        await page.click('text="Next"')
+    } catch (e) {}
+
+    await page.fill('input[name="password"]', testUser.PASSWORD)
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('text="Log in"'),
+    ])
+})
+
+/**
+ * @param {string} str 
+ * @param {string} char 
+ * @returns {number}
+ */
+function secondLastIndexOf(str, char) {
+    const lastIndex = str.lastIndexOf(char)
+    if(lastIndex === -1) return lastIndex
+    return str.lastIndexOf(char, lastIndex - 1)
+}
+
+/**
+ * @param {string} rule 
+ * @returns {string}
+ */
+function getSelector (rule) {
+    return rule.substring(rule.indexOf(' ') + 1, secondLastIndexOf(rule, ' '))
+}
+
+test('current default css rules validity', async () => {
+    for(const defaultRule of Object.values(defaultCSSRules.defaultRules)) {
+        const selector = getSelector(defaultRule)
+        await page.waitForSelector(selector, { timeout: 5000 })
+    }
+})
