b10f95a65bc7158fc6cd82819cfe40e4f1442f28
Raiquen <raiqueng@live.com>
2024-02-23 19:48:59 +0100

    test: CSSRuleParser tests

---
+++ tests/unit/static-data/CSSRuleParserFixtures.js
@@ -0,0 +1,55 @@
+export const CSSFileAsString = `.hide_tweet_analytics div:has(> a[aria-label$='View post analytics']) {
+\tdisplay: none;
+}
+.hide_verified a[href='/i/verified-choose'] {
+\tdisplay: none;
+}
+.hide_subscribe_to_premium div:has(> aside > a[href='/i/verified-choose']) {
+\tdisplay: none;
+}
+.hide_trends_for_you div:has(> div > section > div[aria-label='Timeline: Trending now']) {
+\tdisplay: none;
+}
+.hide_who_to_follow div:has(> div > aside[aria-label='Who to follow']) {
+\tdisplay: none;
+}
+.hide_footer nav[aria-label='Footer'] {
+\tdisplay: none;
+}
+.hide_messages_drawer div[data-testid='DMDrawer'] {
+\tdisplay: none;
+}\n`
+
+export const CSSFileAsStringArr = [
+`.hide_tweet_analytics div:has(> a[aria-label$='View post analytics']) {
+\tdisplay: none;
+}`,
+`.hide_verified a[href='/i/verified-choose'] {
+\tdisplay: none;
+}`,
+`.hide_subscribe_to_premium div:has(> aside > a[href='/i/verified-choose']) {
+\tdisplay: none;
+}`,
+`.hide_trends_for_you div:has(> div > section > div[aria-label='Timeline: Trending now']) {
+\tdisplay: none;
+}`,
+`.hide_who_to_follow div:has(> div > aside[aria-label='Who to follow']) {
+\tdisplay: none;
+}`,
+`.hide_footer nav[aria-label='Footer'] {
+\tdisplay: none;
+}`,
+`.hide_messages_drawer div[data-testid='DMDrawer'] {
+\tdisplay: none;
+}`
+]
+
+export const CSSRulesFormattedArr = [
+    `.hide_tweet_analytics div:has(> a[aria-label$='View post analytics']) {display: none;}`,
+    `.hide_verified a[href='/i/verified-choose'] {display: none;}`,
+    `.hide_subscribe_to_premium div:has(> aside > a[href='/i/verified-choose']) {display: none;}`,
+    `.hide_trends_for_you div:has(> div > section > div[aria-label='Timeline: Trending now']) {display: none;}`,
+    `.hide_who_to_follow div:has(> div > aside[aria-label='Who to follow']) {display: none;}`,
+    `.hide_footer nav[aria-label='Footer'] {display: none;}`,
+    `.hide_messages_drawer div[data-testid='DMDrawer'] {display: none;}`
+]
