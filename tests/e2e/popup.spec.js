6ff9fa6a8719fef59e4f22be0f4ed9178648fb37
Raiquen <raiqueng@live.com>
2024-09-01 17:42:03 +0200

    toggles tests

---
+++ tests/e2e/popup.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from './fixtures'
+import { signIn } from './utils/twitterSignIn'
+// @ts-ignore
+import defaultCSSRules from '../../data/defaultCSSRulesV2.json' assert { type: 'json' }
+import { getRuleName, getToggleName } from '../../src/utils/utils'
+import { getSelector } from './utils/utils'       
+                            
+test('popup', async ({ page, extensionId, context }) => {
+    /**@type {String[]} */    
+    const defaultRulesStr = defaultCSSRules.defaultRules
+                            .filter(ruleObj => ruleObj.UUID !== "2b31e97a-18a7-41cf-aee9-c5ee1842d0fb")
+                            .map(ruleObj => ruleObj.rule)   
+
+    const xPage = await context.newPage()
+    await xPage.setViewportSize({ width: 1920, height: 1080 });
+    await signIn(xPage)
+
+    await page.goto(`chrome-extension://${extensionId}/popup/popup.html`)
+    await page.bringToFront()
+    await page.getByText('Disable All').click()
+
+    await xPage.bringToFront()
+
+    //Toggles
+    for (const defaultRule of defaultRulesStr) {
+        const ruleName = getRuleName(defaultRule)
+        const toggleName = getToggleName(ruleName)
+        const selector = getSelector(defaultRule)
+        await expect(xPage.locator(selector).first()).toBeVisible()
+        await page.getByText(toggleName, { exact: true }).click()
+        await expect(xPage.locator(selector).first()).toBeHidden()
+    }
+
+    //Auto update rules
+
+    //Update now button
+
+    //
+})  
+
