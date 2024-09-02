cafeae9c425e622e6f86d4f8bc1e78f731c72801
Raiquen <raiqueng@live.com>
2024-09-02 20:28:15 +0200

    install and setup babel/eslint-parser and everything required

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,27 @@
+import babelParser from "@babel/eslint-parser";
+
 export default [
     {
+        files: [
+            "**/*.js"
+        ],
+        ignores: [
+            "playwright-report/**", 
+        ],
+        languageOptions: {
+            parser: babelParser,
+            parserOptions: {
+                requireConfigFile: false,
+                babelOptions: {
+                  babelrc: false,
+                  configFile: false,
+                  presets: ["@babel/preset-env"],
+                  plugins: ["@babel/plugin-syntax-import-attributes"]
+                }
+            }
+        },
         rules: {
             "prefer-const": "error"
-        }
+        },
     }
 ]
