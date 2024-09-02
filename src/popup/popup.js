f0e3261cfe6582a81c793c53c5e00fde0b036917
Raiquen Guidotti <raiqueng@live.com>
2024-09-02 15:57:50 +0200

    fix: Update Now button not working  (#143)

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -8,4 +8,5 @@ export { CSSRules as default }
 import('./components/enableDisableAllButton.js')
 import('./components/toggleOptions.js')
 import('./components/autoUpdatesSetting.js')
+import('./components/updateNowButton.js')
 import('./components/editCSSRulesButton.js')
