081d84906445f574c3bbea8b59d1690c0f80f073
Raiquen <raiqueng@live.com>
2024-09-02 20:32:47 +0200

    disabled eslint inline configurations

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,9 @@ export default [
                 }
             }
         },
+        linterOptions: {
+            noInlineConfig: true
+        },
         rules: {
             "prefer-const": "error"
         },
