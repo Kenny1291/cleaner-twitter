c6016dc98e6e11053e8007e335cac942533d519f
Raiquen <raiqueng@live.com>
2024-09-02 23:33:52 +0200

    disabled no-return-assign rule

--- eslint.config.js
+++ eslint.config.js
@@ -115,7 +115,7 @@ export default [
                 "message": "Regex is the root of all evil!"
                 }
             ],
-            "no-return-assign": "error", 
+            // "no-return-assign": "error", 
             "no-script-url": "error", 
             "no-sequences": "error", 
             "no-throw-literal": "error", 
