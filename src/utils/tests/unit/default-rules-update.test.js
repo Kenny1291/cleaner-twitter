51b998c5ccdde2b6be2672b1c8506ee3c8e658b0
Raiquen <raiqueng@live.com>
2024-09-03 16:09:26 +0200

    moved unit tests to dedicated folder

--- src/utils/tests/unit/default-rules-update.test.js
+++ src/utils/tests/unit/default-rules-update.test.js
@@ -9,11 +9,11 @@ import {
     removeRules,
     getCurrentRulesHashed,
     sha256Hash
-} from '../defaultRulesUpdate.js'
-import CSSRulesArrayOfObjectsWithNames from '../../../tests/unit/static-data/CSSRulesArrayOfObjectsWithNames.json' with { type: 'json' }
-import defaultCSSRules from '../../../data/defaultCSSRulesV2.json' with { type: 'json' }
-import defaultRulesV2Hashed from '../../../tests/unit/static-data/default-rules-v2-hashed.json' with { type: 'json' }
-import defaultRulesV2 from '../../../tests/unit/static-data/default-rules-v2.json' with { type: 'json' }
+} from '../../defaultRulesUpdate.js'
+import CSSRulesArrayOfObjectsWithNames from '../../../../tests/unit/static-data/CSSRulesArrayOfObjectsWithNames.json' with { type: 'json' }
+import defaultCSSRules from '../../../../data/defaultCSSRulesV2.json' with { type: 'json' }
+import defaultRulesV2Hashed from '../../../../tests/unit/static-data/default-rules-v2-hashed.json' with { type: 'json' }
+import defaultRulesV2 from '../../../../tests/unit/static-data/default-rules-v2.json' with { type: 'json' }
 import { group } from 'node:console'
 
 describe('getRulesToReplace()', () => {
