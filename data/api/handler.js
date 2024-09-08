f0038710c32ae78c18edda70b971f1f6e809c83b
Raiquen <raiqueng@live.com>
2024-09-08 22:37:18 +0200

    TESTING (GH page as api): delete everything

--- data/api/handler.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-    const defaultCSSRulesV2 = await fetch('https://raw.githubusercontent.com/Kenny1291/cleaner-twitter/main/data/defaultCSSRulesV2.json')
-                                .then(response => response.json())
-    const version = new URLSearchParams(window.location.search).get('v')
-    document.open()
-    document.write(JSON.stringify(defaultCSSRulesV2.oldRules[version]))
-    document.close()
-})()
