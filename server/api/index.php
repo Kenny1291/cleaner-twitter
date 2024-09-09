ebe23a0eb9605ef9dd3d6c5a7a698df3b033f24d
Raiquen <raiqueng@live.com>
2024-09-09 14:03:27 +0200

    TESTING (Vercel PHP api): finished api

--- server/api/index.php
+++ server/api/index.php
@@ -2,4 +2,8 @@
 $defaultCSSRulesV2JSON = file_get_contents('https://raw.githubusercontent.com/Kenny1291/cleaner-twitter/main/data/defaultCSSRulesV2.json');
 $defaultCSSRulesV2OBJ = json_decode($defaultCSSRulesV2JSON);
 $version = $_GET["v"];
-var_dump($defaultCSSRulesV2OBJ);
+$response = new stdClass();
+$response->version = $defaultCSSRulesV2OBJ->version;
+$response->defaultRules = $defaultCSSRulesV2OBJ->defaultRules;
+$response->oldRules = $defaultCSSRulesV2OBJ->oldRules->{$version};
+echo json_encode($response);
