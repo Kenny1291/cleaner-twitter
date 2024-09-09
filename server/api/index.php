0603e665cafcb9d3161f075006c920cb8abd89cb
Raiquen <raiqueng@live.com>
2024-09-09 13:41:51 +0200

    TESTING (Vercel PHP api): typo

--- server/api/index.php
+++ server/api/index.php
@@ -2,4 +2,4 @@
 $defaultCSSRulesV2JSON = file_get_contents('https://raw.githubusercontent.com/Kenny1291/cleaner-twitter/main/data/defaultCSSRulesV2.json');
 $defaultCSSRulesV2OBJ = json_decode($defaultCSSRulesV2);
 $version = $_GET["v"];
-var_dump(defaultCSSRulesV2OBJ);
+var_dump($defaultCSSRulesV2OBJ);
