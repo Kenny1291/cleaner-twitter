4707dd37141814e263772937f79debee35de4621
Raiquen Guidotti <raiqueng@live.com>
2024-09-09 18:34:16 +0200

    fix: server respond even if version does not exist (#189)

--- server/api/index.php
+++ server/api/index.php
@@ -5,6 +5,6 @@ $version = $_GET["v"];
 $response = new stdClass();
 $response->version = $defaultCSSRulesV2OBJ->version;
 $response->defaultRules = $defaultCSSRulesV2OBJ->defaultRules;
-$response->oldRules = $defaultCSSRulesV2OBJ->oldRules->{$version};
+$response->oldRules = $defaultCSSRulesV2OBJ->oldRules?->{$version};
 header('Access-Control-Allow-Origin: *');
 echo json_encode($response);
