eb773667df28570c9b26172359f639326e19f80a
Raiquen <raiqueng@live.com>
2024-09-11 20:45:34 +0200

    rename field "retries" to tries

--- src/utils/RetryHandler/RetryHandler.js
+++ src/utils/RetryHandler/RetryHandler.js
@@ -3,30 +3,30 @@ import TooManyAttemptsError from "./TooManyAttemptsError.js"
 export default class RetryHandler {
     #attempts = 0
     #fn
-    #retries
+    #tries
     #delay
 
     /**
      * @param {function} fn - Function to call
-     * @param {number} retries - (Int) Max number of retries
-     * @param {number} delay - (Int) The delay in milliseconds between each retry
+     * @param {number} tries - (Int) Max number of tries. (Defaults to 3)
+     * @param {number} delay - (Int) The delay in milliseconds between each retry. (Defaults to 1000)
      */
-    constructor(fn, retries = 3, delay = 1000) {
-        if (retries < 1) throw new RangeError("retries must be >= 1")
+    constructor(fn, tries = 3, delay = 1000) {
+        if (tries < 1) throw new RangeError("tries must be >= 1")
         if (delay < 1) throw new RangeError("delay must be >= 1")
         this.#fn = fn
-        this.#retries = Math.trunc(retries)
+        this.#tries = Math.trunc(tries)
         this.#delay = Math.trunc(delay)
     }
 
     async run() {
-        while (this.#attempts < this.#retries) {
+        while (this.#attempts < this.#tries) {
             try {
                 return await this.#fn()
             } catch (error) {
                 this.#attempts++
-                if (this.#attempts >= this.#retries) {
-                    throw new TooManyAttemptsError(`${this.#fn.name} exceeded max number of retries (${this.#retries})`)
+                if (this.#attempts >= this.#tries) {
+                    throw new TooManyAttemptsError(`${this.#fn.name || "Anonymous function"} exceeded max number of tries (${this.#tries})`)
                 }
                 await this.#sleep(this.#delay)
             }
