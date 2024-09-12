b04ee5731ce431d310037de6ffde6f45047d6dbe
Raiquen <raiqueng@live.com>
2024-09-12 19:59:29 +0200

    fix: avoid undefined warning $_GET

--- server/api/index.php
+++ server/api/index.php
@@ -2,7 +2,7 @@
 $defaultCSSRulesV2JSON = file_get_contents('https://raw.githubusercontent.com/Kenny1291/cleaner-twitter/main/data/defaultCSSRulesV2.json');
 $defaultCSSRulesV2OBJ = json_decode($defaultCSSRulesV2JSON);
 
-$version = $_GET["v"];
+$version = $_GET["v"] ?? null;
 $response = new stdClass();
 $response->version = $defaultCSSRulesV2OBJ->version;
 $response->defaultRules = $defaultCSSRulesV2OBJ->defaultRules;
