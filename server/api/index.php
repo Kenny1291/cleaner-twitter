16be1c7486bc3ef89dcb690216c6e52e6b046b37
Raiquen <raiqueng@live.com>
2024-09-12 19:44:40 +0200

    server: include also version

--- server/api/index.php
+++ server/api/index.php
@@ -4,15 +4,12 @@ $defaultCSSRulesV2OBJ = json_decode($defaultCSSRulesV2JSON);
 
 $version = $_GET["v"];
 $response = new stdClass();
-if ($version) {
     $response->version = $defaultCSSRulesV2OBJ->version;
     $response->defaultRules = $defaultCSSRulesV2OBJ->defaultRules;
+if ($version) {
     if (isset($defaultCSSRulesV2OBJ->oldRules->{$version})) {
         $response->oldRules = $defaultCSSRulesV2OBJ->oldRules->{$version};
     }
-} else {
-    $response = $defaultCSSRulesV2OBJ->defaultRules;
 }
-
 header('Access-Control-Allow-Origin: *');
 echo json_encode($response);
