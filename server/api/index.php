73bf975cfde3ffdaa93db0982a3862643f917411
Raiquen <raiqueng@live.com>
2024-09-12 19:39:20 +0200

    feat: server api: send only default rules if  no query string present

--- server/api/index.php
+++ server/api/index.php
@@ -1,12 +1,18 @@
 <?php
 $defaultCSSRulesV2JSON = file_get_contents('https://raw.githubusercontent.com/Kenny1291/cleaner-twitter/main/data/defaultCSSRulesV2.json');
 $defaultCSSRulesV2OBJ = json_decode($defaultCSSRulesV2JSON);
+
 $version = $_GET["v"];
 $response = new stdClass();
-$response->version = $defaultCSSRulesV2OBJ->version;
-$response->defaultRules = $defaultCSSRulesV2OBJ->defaultRules;
-if (isset($defaultCSSRulesV2OBJ->oldRules->{$version})) {
-    $response->oldRules = $defaultCSSRulesV2OBJ->oldRules->{$version};
+if ($version) {
+    $response->version = $defaultCSSRulesV2OBJ->version;
+    $response->defaultRules = $defaultCSSRulesV2OBJ->defaultRules;
+    if (isset($defaultCSSRulesV2OBJ->oldRules->{$version})) {
+        $response->oldRules = $defaultCSSRulesV2OBJ->oldRules->{$version};
+    }
+} else {
+    $response = $defaultCSSRulesV2OBJ->defaultRules;
 }
+
 header('Access-Control-Allow-Origin: *');
 echo json_encode($response);
