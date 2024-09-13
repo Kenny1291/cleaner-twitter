46b18b77219157340e479d0ec47bdfd7da606a41
Raiquen <raiqueng@live.com>
2024-09-13 15:46:36 +0200

    TESTING: route to write log to DB

--- server/api/functions.php
+++ server/api/functions.php
@@ -17,5 +17,44 @@ function data() {
 
 #[Route('/logs/collect')]
 function collectLogs() {
-    echo "Collect Logs";
+    //Read raw data from the POST request body
+    $error = file_get_contents('php://input');
+    
+    if (!json_validate($error)) {
+        http_response_code(400);
+        echo '400 Bad Request';
+        return;
+    }
+
+    $data = [
+        'requests' => [
+            [
+                'type' => 'execute', 
+                'stmt' => [
+                    'sql' => "INSERT INTO logs (error) VALUES @error",
+                    "named_args" => [
+                        [
+                            "name" => "error",
+                            "value" => [
+                                "type" => "text",
+                                "value" => $error 
+                            ]
+                        ],
+                        
+                    ]
+                ]
+            ],
+            ['type' => 'close']
+        ]
+    ];
+    $ch = curl_init($_ENV['TURSO_DB_HTTP_URL']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $_ENV['TURSO_DB_AUTH_TOKEN'],
+        'Content-Type: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_exec($ch);
+    curl_close($ch);
 }
