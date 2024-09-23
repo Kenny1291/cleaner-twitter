1bb9d9ef4aed0b006d949682ea68f1e625420e6c
Raiquen <raiqueng@live.com>
2024-09-23 11:33:13 +0200

    var_dump instead of echo

--- server/api/functions.php
+++ server/api/functions.php
@@ -94,7 +94,7 @@ function test() {
     //         response400();
     // }
 
-    echo array_diff_assoc(['Accept' => 'text/plain'], getallheaders());
+    var_dump(array_diff_assoc(['Accept' => 'text/plain'], getallheaders()));
 
     // echo $_ENV['LOG_KEY'];
 }
