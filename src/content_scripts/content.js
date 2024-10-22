251e76eb1bc4d68234c18922c659a6fdde41aa15
Raiquen <raiqueng@live.com>
2024-10-22 12:41:58 +0200

    import utils into content script

--- src/content_scripts/content.js
+++ src/content_scripts/content.js
@@ -1,47 +1,49 @@
-import { getRuleWithUniqueClass, getRuleUniqueName } from '../utils/utils.js'
+(async () => {
+    const utils = await import('../utils/utils.js');
 
-/**
- * Call when content.js is injected (when Twitter is visited)
- */
-injectStylesAndSetClasses()
+    /**
+     * Call when content.js is injected (when Twitter is visited)
+     */
+    injectStylesAndSetClasses()
 
-/**
- * Listens for messages from tabs
- */
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    /**@type {string} */
-    const messageName = Object.keys(message)[0]
+    /**
+     * Listens for messages from tabs
+     */
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        /**@type {string} */
+        const messageName = Object.keys(message)[0]
 
-    /**@type {boolean} */
-    const messageState = Object.values(message)[0]
+        /**@type {boolean} */
+        const messageState = Object.values(message)[0]
 
-    if(messageName === 'rulesChanged') {
-        injectStylesAndSetClasses()
-    } else {
-        document.body.classList.toggle(messageName, messageState)
-    }
-})
-
-/**
- * Injects CSS styles into the page (body) and sets classes based on the stored CSS rules.
- */
-function injectStylesAndSetClasses() {
-    /**@type {?HTMLStyleElement} */
-    // @ts-ignore
-    const oldStyle =  document.getElementById('cleanerTwitterStyles')
-
-    if(oldStyle) oldStyle.remove()
-
-    /**@type {HTMLStyleElement} */
-    const style = document.createElement('style')
-
-    style.id = 'cleanerTwitterStyles'
-
-    chrome.storage.sync.get().then(result => {
-        result.CSSRulesArrayOfObjectsWithNames.forEach(CSSRule => {
-            style.innerHTML += getRuleWithUniqueClass(CSSRule)
-            document.body.classList.toggle(getRuleUniqueName(CSSRule), CSSRule.active )
-        })
+        if(messageName === 'rulesChanged') {
+            injectStylesAndSetClasses()
+        } else {
+            document.body.classList.toggle(messageName, messageState)
+        }
     })
-    document.head.appendChild(style)
-}
+
+    /**
+     * Injects CSS styles into the page (body) and sets classes based on the stored CSS rules.
+     */
+    function injectStylesAndSetClasses() {
+        /**@type {?HTMLStyleElement} */
+        // @ts-ignore
+        const oldStyle =  document.getElementById('cleanerTwitterStyles')
+
+        if(oldStyle) oldStyle.remove()
+
+        /**@type {HTMLStyleElement} */
+        const style = document.createElement('style')
+
+        style.id = 'cleanerTwitterStyles'
+
+        chrome.storage.sync.get().then(result => {
+            result.CSSRulesArrayOfObjectsWithNames.forEach(CSSRule => {
+                style.innerHTML += utils.getRuleWithUniqueClass(CSSRule)
+                document.body.classList.toggle(utils.getRuleUniqueName(CSSRule), CSSRule.active)
+            })
+        })
+        document.head.appendChild(style)
+    }
+})()
