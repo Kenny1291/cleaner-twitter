50e9a22b7dc6c0b783cbcfa6c9acc61e9e76bcd6
Raiquen <raiqueng@live.com>
2025-01-19 12:46:04 +0100

    fix: move RetryHandler in utils.js to solve "Failed to fetch dynamically imported module

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,3 @@
-import RetryHandler from "./RetryHandler/RetryHandler.js"
-
 /**
  * @param {string} key
  * @returns {Promise<{[key: string]: any}>}
@@ -174,4 +172,56 @@ export function makeNamedFn(fn, ...args) {
             return fn(...args)
         }
     }[fn.name]
+}
+
+class RetryHandler {
+    #attempts = 0
+    #fn
+    #tries
+    #delay
+
+    /**
+     * @param {function} fn - Function to call
+     * @param {number} tries - (Int) Max number of tries. (Defaults to 3)
+     * @param {number} delay - (Int) The delay in milliseconds between each retry. (Defaults to 1000)
+     */
+    constructor(fn, tries = 3, delay = 1000) {
+        if (tries < 1) throw new RangeError("tries must be >= 1")
+        if (delay < 1) throw new RangeError("delay must be >= 1")
+        this.#fn = fn
+        this.#tries = Math.trunc(tries)
+        this.#delay = Math.trunc(delay)
+    }
+
+    async run() {
+        while (this.#attempts < this.#tries) {
+            try {
+                return await this.#fn()
+            } catch (error) {
+                this.#attempts++
+                if (this.#attempts >= this.#tries) {
+                    throw new TooManyAttemptsError(`${this.#fn.name || "Anonymous function"} exceeded max number of tries (${this.#tries})`)
+                }
+                await this.#sleep(this.#delay)
+            }
+        }
+    }
+
+    /**
+     * @param {number} time - Milliseconds
+     * @returns {Promise}
+     */
+    #sleep(time) {
+        return new Promise(resolve => setTimeout(resolve, time))
+    }
+}
+
+class TooManyAttemptsError extends Error {
+    /**
+     * @param {string} message
+     */
+    constructor(message) {
+        super(message)
+        this.name = 'TooManyAttemptsError'
+    }
 }
