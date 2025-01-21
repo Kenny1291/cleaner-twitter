d14f5d21cf1b32030ba3ed529f84b1417a0d74fc
Raiquen <raiqueng@live.com>
2025-01-21 12:25:50 +0100

    chore: delete RetryHandler files

--- src/utils/RetryHandler/TooManyAttemptsError.js
+++
@@ -1,9 +0,0 @@
-export default class TooManyAttemptsError extends Error {
-    /**
-     * @param {string} message
-     */
-    constructor(message) {
-        super(message)
-        this.name = 'TooManyAttemptsError'
-    }
-}
